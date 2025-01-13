82c89dafc018f06e47043de8d01da94c65215655
Rifqi Sulthan Nazhmi <156320563+Nazhmi25@users.noreply.github.com>
2025-01-14 02:40:47 +0700

    Add files via upload

---
+++ tokoroti/about.php
@@ -0,0 +1,17 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">HABANMI BAKERY adalah rantai toko roti Indonesia terkemuka dengan 22 cabang (dapur pusat) yang mengelola lebih dari 400 outlet: Jabodetabek (Gajah Mada, Pondok Pinang, Jatinegara, Cikini, Sunter, Serpong, Ciputat, Pekayon, Bogor dan Karawang), Bandung, Surabaya, Lampung, Batam, Pekanbaru, Makassar, Manado, Bali, Solo, Semarang, Balikpapan, dan Samarinda. Kami masih terus memperluas secara nasional ke kota-kota lain.</p>
+<p>
+HABANMI BAKERY adalah salah satu bisnis roti modern di Indonesia. Didirikan pada tahun 2024, saat ini dikelola di bawah PT. UJIAN PWEB. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
